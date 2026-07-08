d89246e0dfe6bbbe838619c8252caacd9b4ca086
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-08 12:55:35 +0500

    backend completed with some optimization

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-import express from "express"
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import connectDb from "./src/config/db.js";
 import notesRoutes from "./src/routes/notesRoutes.js";
 
 const app = express();
-dotenv.config()
-connectDb()
+dotenv.config();
 
-app.use(express.json())
-app.use('/api/notes', notesRoutes)
+app.use(express.json());
+app.use("/api/notes", notesRoutes);
 
-app.listen (5001, () => {
-    console.log("Server is running on PORT 5001")
-})
+connectDb().then(() => {
+  app.listen(5001, () => {
+    console.log("Server is running on PORT 5001");
+  });
+});
